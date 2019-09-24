c0599bb20ed2255466751234e20618f6f5e4cbad
rumblefrog <contact@zhenyangli.me>
2019-09-24 13:48:49 -0400

    switch to steamid-ng lib

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use structopt::StructOpt;
-
-mod steamid;
+use steamid_ng::SteamID;
 
 #[derive(StructOpt, Debug)]
 struct Cli {
@@ -10,5 +9,19 @@ struct Cli {
 fn main() {
     let args = Cli::from_args();
 
-    println!("{:?}", args);
+    let id: SteamID;
+
+    if args.id.contains("STEAM") {
+       id = SteamID::from_steam2(&args.id).unwrap();
+    } else if args.id.contains("[") {
+        id = SteamID::from_steam3(&args.id).unwrap();
+    } else if args.id.parse::<u64>().is_ok() {
+        id = SteamID::from(args.id.parse::<u64>().unwrap());
+    } else {
+        return
+    }
+
+    println!("SteamID64: {}", u64::from(id));
+    println!("SteamID2: {}", id.steam2());
+    println!("SteamID3: {}", id.steam3());
 }
